817f1432c5e092b27d8fc7891e48071fb6d4f69e
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-09 17:26:10 -0300

    keep going

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import {AppPipesModule} from '../pipes/pipes.module';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { HomePage } from './home.page';
         path: '',
         component: HomePage
       }
-    ])
+    ]),
+    AppPipesModule,
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data/data.service';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  people: Array<any> = [];
+  constructor(private dataService: DataService, private navCtrl: NavController, private authService: AuthService) {
+  }
 
+  ngOnInit() {
+    this.dataService.getPeopleList()
+      .subscribe((people: any) => {
+        this.people = people;
+      });
+  }
+
+  openPerson(index) {
+    this.navCtrl.navigateForward(`/person/${index}`);
+  }
 }
